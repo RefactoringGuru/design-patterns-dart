686b628e0b7a0d0a674b6682927669a7df1dd8f4
ilopX <ilopXgroup@gmail.com>
2021-12-11 17:02:02 +0200

    Rename folder shape_engine to text_graphics

--- bin/adapter/text_graphics/engine/shape_engine.dart
+++ bin/adapter/text_graphics/engine/shape_engine.dart
@@ -14,7 +14,7 @@ class ShapeEngine {
   });
 
   String render() {
-    final can = Canvas(width, height, 3);
+    final can = Canvas(width, height, 2);
     for (final Shape shape in shapes) {
       can
         ..translate = Point(shape.x, shape.y)
