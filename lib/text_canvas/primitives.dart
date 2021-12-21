43c1eb9765ca3eabfcdeaf27805c0ba959bb3370
ilopX <ilopXgroup@gmail.com>
2021-12-21 15:19:42 +0200

    Move BorderStyle to primitives.dart

--- lib/text_canvas/primitives.dart
+++ lib/text_canvas/primitives.dart
@@ -19,3 +19,64 @@ class Point {
 
   Point(this.x, this.y);
 }
+
+class BorderStyle {
+  static final BorderStyle bold = BorderStyle.fromBorderText(
+    '▄▄▄▄'
+    '█  █'
+    '▀▀▀▀',
+  );
+
+  static final BorderStyle double = BorderStyle.fromBorderText(
+    '╔══╗'
+    '║  ║'
+    '╚══╝',
+  );
+
+  static final BorderStyle single = BorderStyle.fromBorderText(
+    '┌──┐'
+    '│  │'
+    '└──┘',
+  );
+
+  static final BorderStyle round = BorderStyle.fromBorderText(
+    '╭━━╮'
+    '    '
+    '    ',
+  );
+
+  final String topLeft;
+  final String topRight;
+  final String bottomRight;
+  final String bottomLeft;
+
+  final String top;
+  final String bottom;
+
+  final String left;
+  final String right;
+
+  BorderStyle._(
+    this.topLeft,
+    this.topRight,
+    this.bottomRight,
+    this.bottomLeft,
+    this.top,
+    this.bottom,
+    this.left,
+    this.right,
+  );
+
+  factory BorderStyle.fromBorderText(String text) {
+    return BorderStyle._(
+      text[0],
+      text[3],
+      text[11],
+      text[8],
+      text[1],
+      text[9],
+      text[4],
+      text[7],
+    );
+  }
+}
