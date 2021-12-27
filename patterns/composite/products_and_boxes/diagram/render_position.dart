896e395e9da653e05af3c68500685ae87fa6930d
ilopX <ilopXgroup@gmail.com>
2021-12-27 14:46:58 +0200

    Add RenderPosition

---
+++ patterns/composite/products_and_boxes/diagram/render_position.dart
@@ -0,0 +1,30 @@
+import 'package:design_patterns_dart/text_canvas/canvas.dart';
+import 'package:design_patterns_dart/text_canvas/primitives.dart';
+
+import 'render_element.dart';
+
+class RenderPosition extends RenderElement {
+  final int x;
+  final int y;
+  final RenderElement child;
+
+  RenderPosition({
+    required this.x,
+    required this.y,
+    required this.child,
+  });
+
+  @override
+  int get height => child.height;
+
+  @override
+  int get width => child.width;
+
+  @override
+  void render(Canvas dc) {
+    final oldTranslate = dc.translate;
+    dc.translate = Point(x, y);
+    child.render(dc);
+    dc.translate = oldTranslate;
+  }
+}
