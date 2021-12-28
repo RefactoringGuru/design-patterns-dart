1dd2932aaa8b8404037ff1659a5c9da56fc93dea
ilopX <ilopXgroup@gmail.com>
2021-12-28 14:15:21 +0200

    Add renderElement converter

---
+++ patterns/composite/products_and_boxes/diagram/convert_product_to_render_element.dart
@@ -0,0 +1,30 @@
+import 'package:design_patterns_dart/text_canvas.dart';
+
+import '../products/product.dart';
+import '../products/product_leaf.dart';
+import 'render_element.dart';
+import 'render_text.dart';
+
+extension ConvertProductToDiagram on Product {
+  RenderElement toRenderElement() {
+    return RenderText(
+      content,
+      borderStyle: borderStyleBySize(),
+    );
+  }
+
+  BorderStyle borderStyleBySize() {
+    // if (size > 4) {
+    //   return BorderStyle.bold;
+    // } else
+    // if (size >= 2) {
+    //   return BorderStyle.bold;
+    // } else
+
+    if (this is ProductLeaf) {
+      return BorderStyle.empty;
+    } else {
+      return BorderStyle.single;
+    }
+  }
+}
