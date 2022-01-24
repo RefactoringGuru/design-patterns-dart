382fb09e1d794dd536183bc32148aa66bac4578c
ilopX <ilopXgroup@gmail.com>
2022-01-24 14:07:35 +0200

    Add render method for ImageEditor.

--- patterns/composite/image_editor/editor/image_editor.dart
+++ patterns/composite/image_editor/editor/image_editor.dart
@@ -12,9 +12,11 @@ class ImageEditor {
     _allShapes
       ..clear()
       ..addAll(shapes);
+  }
 
+  String render() {
     final graphics = Graphics(_allShapes.width + 2, _allShapes.height + 2);
     _allShapes.paint(graphics);
-    print(graphics.toString());
+    return graphics.toString();
   }
 }
