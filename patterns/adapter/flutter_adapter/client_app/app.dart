a5878ce1783176be2678c37feb08ee9f90ba0f2a
ilopX <ilopXgroup@gmail.com>
2022-02-02 14:58:43 +0200

    Add "NextColorEvent".

--- patterns/adapter/flutter_adapter/client_app/app.dart
+++ patterns/adapter/flutter_adapter/client_app/app.dart
@@ -1,6 +1,9 @@
 import 'dart:ui';
 
+import '../../../observer/app_observer/observer/event.dart';
 import '../adapter/classic_app.dart';
+
+class NextTextColorEvent extends Event {}
 
 class App extends ClassicAppBase {
   final maxTextSize = 150;
@@ -34,6 +37,11 @@ class App extends ClassicAppBase {
   @override
   void onWheel(double deltaX, double deltaY) {
     textSize += deltaY ~/ 10;
+  }
+
+  @override
+  void onMouseDown() {
+    events.notify(NextTextColorEvent());
   }
 
   @override
--- patterns/adapter/flutter_adapter/client_app/flutter_text_property_widget.dart
+++ patterns/adapter/flutter_adapter/client_app/flutter_text_property_widget.dart
@@ -29,20 +29,43 @@ class _FlutterTextPropertyWidgetState extends State<FlutterTextPropertyWidget> {
       ],
     );
   }
+  final colors = [
+    Colors.black,
+    Colors.pink.shade600,
+    Colors.deepPurple.shade600,
+    Colors.blue.shade600,
+    Colors.green.shade600,
+  ];
 
   late EventFunction<ClassicAppRepaint> _classicAppRepaintEvent;
+  late EventFunction<NextTextColorEvent> _nextColorEvent;
 
   @override
   void initState() {
     _classicAppRepaintEvent = widget.classicApp.events.subscribe(
       (ClassicAppRepaint e) => setState(() {}),
     );
+
+    _nextColorEvent = widget.classicApp.events.subscribe(
+      (NextTextColorEvent e) => setState(() {
+        final currColor =  widget.classicApp.textColor;
+        var nextIndex = colors.indexOf(currColor)+1;
+
+        if (nextIndex >= colors.length) {
+          nextIndex = 0;
+        }
+
+        widget.classicApp.textColor = colors[nextIndex];
+      }),
+    );
     super.initState();
   }
 
   @override
   void dispose() {
-    widget.classicApp.events.unsubscribe(_classicAppRepaintEvent);
+    widget.classicApp.events
+      ..unsubscribe(_classicAppRepaintEvent)
+      ..unsubscribe(_nextColorEvent);
     super.dispose();
   }
 
@@ -51,7 +74,10 @@ class _FlutterTextPropertyWidgetState extends State<FlutterTextPropertyWidget> {
       children: [
         SizedBox(
           width: 31,
-          child: Text(app.textSize.toString().padLeft(3, ' ')),
+          child: Text(
+            app.textSize.toString(),
+            textAlign: TextAlign.right,
+          ),
         ),
         SizedBox(
           width: 200,
@@ -69,11 +95,6 @@ class _FlutterTextPropertyWidgetState extends State<FlutterTextPropertyWidget> {
   }
 
   Widget _buildTextColorButtons(App app) {
-    final colors = [
-      Colors.black,
-      Colors.pink.shade600,
-      Colors.deepPurple.shade600,
-    ];
     return Row(
       children: [
         ...colors.map(
