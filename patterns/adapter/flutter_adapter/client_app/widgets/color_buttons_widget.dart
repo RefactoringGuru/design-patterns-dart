cb086cf8e4fd2593d1e84dad077a4f94da87bb62
ilopX <ilopXgroup@gmail.com>
2022-02-06 10:39:48 +0200

    Add color buttons widget.

---
+++ patterns/adapter/flutter_adapter/client_app/widgets/color_buttons_widget.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+
+class ColorButtonsWidget extends StatelessWidget {
+  final Color currentColor;
+  final List<Color> colors;
+  final void Function(Color color) onColorSelect;
+
+  const ColorButtonsWidget({
+    Key? key,
+    required this.currentColor,
+    required this.colors,
+    required this.onColorSelect,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        ...colors.map(
+              (Color color) {
+            return _buildColorButton(color);
+          },
+        ).toList(),
+      ],
+    );
+  }
+
+  Widget _buildColorButton(Color color) {
+    final isColorSelect = (color == currentColor);
+    return GestureDetector(
+      onTap: () {
+        onColorSelect(color);
+      },
+      child: Container(
+        width: 20,
+        height: 20,
+        color: color,
+        child: isColorSelect ? _buildSelectColorIcon() : null,
+      ),
+    );
+  }
+
+  Widget _buildSelectColorIcon() {
+    return Center(
+      child: Container(
+        width: 4,
+        height: 4,
+        color: Colors.white.withOpacity(0.8),
+      ),
+    );
+  }
+}
