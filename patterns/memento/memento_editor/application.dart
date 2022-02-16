46283bbb4bc5aaf923ca3ac68836d6c5a0985aae
ilopX <ilopXgroup@gmail.com>
2022-02-16 13:22:23 +0200

    Remove initialization to MementoEditorApplication.

--- patterns/memento/memento_editor/application.dart
+++ patterns/memento/memento_editor/application.dart
@@ -1,7 +1,19 @@
+import 'dart:math';
+
 import 'editor/editor.dart';
 import 'memento_pattern/snapshot.dart';
+import 'shapes/shape.dart';
 
 class MementoEditorApplication {
   final editor = Editor();
   final snapshots = <Snapshot>[];
+
+  MementoEditorApplication() {
+    const positionRadius = 300.0;
+    for (var i = 0; i < 7; i++) {
+      final x = 60 + positionRadius + cos(i / 1.15) * positionRadius;
+      final y = 60 + positionRadius + sin(i / 1.15) * positionRadius;
+      editor.shapes.add(Shape(x, y));
+    }
+  }
 }
--- patterns/memento/memento_editor/editor/editor.dart
+++ patterns/memento/memento_editor/editor/editor.dart
@@ -1,22 +1,11 @@
-import 'dart:math';
 import 'dart:ui';
 
 import '../../../adapter/flutter_adapter/classic_app/classic_app.dart';
 import '../memento_pattern/originator.dart';
-import '../shapes/shape.dart';
-import 'manipulator.dart';
 import '../shapes/shapes.dart';
+import 'manipulator.dart';
 
 class Editor extends ClassicApp with Manipulator, Shapes, Originator {
-  Editor() {
-    const positionRadius = 300.0;
-    for (var i = 0; i < 7; i++) {
-      final x = 60 + positionRadius + cos(i / 1.15) * positionRadius;
-      final y = 60 + positionRadius + sin(i / 1.15) * positionRadius;
-      shapes.add(Shape(x, y));
-    }
-  }
-
   @override
   void onPaint(Canvas canvas, Size canvasSize) {
     _paintBackground(canvas, canvasSize);
