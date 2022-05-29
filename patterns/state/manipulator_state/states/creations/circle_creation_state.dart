bc8e6d15cbe4f1f7090482d26c73ced2d6a8025a
ilopX <ilopXgroup@gmail.com>
2022-05-29 19:30:35 +0300

    Add default width CircleCreationState.

--- patterns/state/manipulator_state/states/creations/circle_creation_state.dart
+++ patterns/state/manipulator_state/states/creations/circle_creation_state.dart
@@ -5,7 +5,7 @@ import 'creation_state.dart';
 class CircleCreationState extends CreationState {
   @override
   Shape createShape(double x, double y) {
-    return CircleShape(x, y, 100, 100);
+    return CircleShape(x, y, 100, 100, 25);
   }
 
   @override
