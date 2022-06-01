f7c6fba299e90f2b48b30f1625655400b1a9e1da
ilopX <ilopXgroup@gmail.com>
2022-06-01 23:30:27 +0300

    Fix height changer.

--- patterns/state/manipulator_state/shapes/text_shape.dart
+++ patterns/state/manipulator_state/shapes/text_shape.dart
@@ -5,16 +5,26 @@ import '../states/selections/text_resize_state.dart';
 import 'base_shape.dart';
 
 class TextShape extends BaseShape {
-  TextShape(super.x, super.y, super.width, super.height) {
-    _buildParagraph();
-  }
-
-  set text(String newText) {
-    _text = newText;
-    _buildParagraph();
+  TextShape(double x, double y, double height) : super(x, y, 0, height) {
+    _buildParagraph(height);
   }
 
   String get text => _text;
+
+  set text(String newText) {
+    _text = newText;
+    _buildParagraph(_textHeight);
+  }
+
+  double get userHeight => _textHeight;
+
+  Paragraph get paragraph => _paragraph;
+
+  @override
+  Rect get rect {
+    final fixY = height - _textHeight;
+    return Rect.fromLTWH(x, y + fixY, width, height - fixY);
+  }
 
   @override
   void paint(Canvas can) {
@@ -27,40 +37,38 @@ class TextShape extends BaseShape {
   }
 
   @override
-  double get width => _paragraph.maxIntrinsicWidth;
-
-  @override
-  Rect get rect => Rect.fromLTWH(x, y, width, height);
-
-  @override
   void resize(double _, double newHeight) {
     if (newHeight < 2) {
       newHeight = 2;
     }
 
-    super.resize(_, newHeight);
-    final prevWidth = width;
-    _buildParagraph();
-    final centerX = x + (prevWidth - width) / 2;
+    final oldWidth = width;
+    _buildParagraph(newHeight);
+    final centerX = x - (width - oldWidth) / 2;
     move(centerX, y);
   }
 
-  void _buildParagraph() {
+  void _buildParagraph(double newHeight) {
+    _textHeight = newHeight;
+
     final style = ParagraphStyle(
       textDirection: TextDirection.ltr,
     );
     final tStyle = TextStyle(
       fontFamily: 'Arial',
       color: Color(0xffffffff),
-      fontSize: height,
+      fontSize: newHeight,
     );
     _paragraph = (ParagraphBuilder(style)
           ..pushStyle(tStyle)
           ..addText(_text))
         .build();
     _paragraph.layout(ParagraphConstraints(width: double.infinity));
+
+    super.resize(_paragraph.maxIntrinsicWidth, _paragraph.height);
   }
 
   late Paragraph _paragraph;
   String _text = 'Text';
+  late double _textHeight;
 }
